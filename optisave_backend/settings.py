c753435fbdc6cd31b29cd29f324bc5aaafcfd08a
Kacper Sokołowski <kontakt@kaapa.pl>
2018-11-24 17:23:12 +0100

    Add static

--- optisave_backend/settings.py
+++ optisave_backend/settings.py
@@ -130,4 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+
+STATIC_ROOT = '/home/ubuntu/optisave-backend/optisave-backend/static'
 STATIC_URL = '/static/'
